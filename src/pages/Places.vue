f97b716d17fcfe2774092b300df283b83dccd647
Tomer Shalev <tshalev@proofpoint.com>
2021-09-11 15:22:46 +0300

    Add places page

---
+++ src/pages/Places.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <locations />
+  </q-page>
+</template>
+
+<script>
+import Locations from 'components/Locations.vue';
+
+export default {
+  components: { Locations },
+}
+</script>
