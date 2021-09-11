ca118a5c0bba947728717bf237fc3719c4e962c7
Tomer Shalev <tshalev@proofpoint.com>
2021-09-11 15:19:13 +0300

    Remove dead code

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,14 @@
 <template>
   <q-page class="row items-center justify-between">
-    <!--<h1 class="text-white">Basic</h1>-->
-      <!--<example-component
-        title="Example component"
-        active
-        :todos="todos"
-        :meta="meta"
-      ></example-component>-->
   </q-page>
 </template>
 
 <script lang="ts">
 import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/CompositionComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
-  // components: { ExampleComponent },
   setup() {
     const todos = ref<Todo[]>([
       {
