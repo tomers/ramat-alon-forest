a315be77436dda57272ea87e8b302e99c1bfb764
Tomer Shalev <tshalev@proofpoint.com>
2021-09-11 15:39:14 +0300

    Update monster

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="bg-white q-pa-sm">
-      <h3 class="text-black text-bold">ברוכים הבאים</h3>
+      <q-img src="../assets/welcome.jpg" />
       <h6>
         צוות יער רמת אלון מודה לכם שבאתם לטייל בפינת החמד אותה אנחנו מטפחים
       </h6>
