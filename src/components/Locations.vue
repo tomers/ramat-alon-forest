45228269204507452f8465d38f59a9317482be43
Tomer Shalev <tshalev@proofpoint.com>
2022-09-28 22:39:04 +0300

    Update locations

--- src/components/Locations.vue
+++ src/components/Locations.vue
@@ -4,7 +4,33 @@
       <q-item-label
         header
       >
-        נקודות במסלול
+        מסלולים
+      </q-item-label>
+
+      <q-item
+        v-for="route in routes"
+        :key="route.title"
+        clickable
+        :to="route.to"
+      >
+        <q-item-section
+          avatar
+        >
+          <q-icon name="route" color="primary" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ route.title }}</q-item-label>
+          <q-item-label caption v-if="route.caption">
+            {{ route.caption }}
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item-label
+        header
+      >
+        נקודות עניין
       </q-item-label>
 
       <q-item
@@ -31,30 +57,47 @@
 </template>
 
 <script>
+const routes = [
+  {
+    title: 'שביל ואדיות חיפה (ירוק-אדום)',
+    caption: 'מטיילת רמת אלון דרך ואדי בן-דור לסביוני דניה',
+    to: ''
+  },
+  {
+    title: 'שביל העץ הקסום (צהוב-לבן)',
+    caption: 'מטיילת רמת אלון לפינת היער מעל מגה בעיר',
+    to: ''
+  },
+  {
+    title: 'שביל התצפית (אדום-לבן)',
+    caption: 'מהעץ הקסום לבריכת המים מעל הטכניון',
+    to: ''
+  },
+]
 const places = [
   {
     title: 'פינת היער',
-    caption: 'יציאה עליונה לשכונה',
+    caption: 'כניסה עליונה ליער, מעל מגה בעיר',
     to: ''
   },
   {
     title: 'טיילת רמת אלון',
-    caption: 'יציאה תחתונה לשכונה',
-    to: ''
-  },
-  {
-    title: 'ואדי שנאק',
-    caption: 'מסלול ירוק',
-    to: ''
-  },
-  {
-    title: 'ואדי בן-דור',
-    caption: 'מסלול אדום-ירוק',
+    caption: 'כניסה תחתונה ליער, בסוף הטיילת',
     to: ''
   },
   {
     title: 'העץ הקסום',
-    caption: 'אטרקציה',
+    caption: 'נקודת מנוחה מוצלת בצילו של אלון גדול במסלול העץ הקסום',
+    to: ''
+  },
+  {
+    title: 'האלון הגדול',
+    caption: 'נקודת מנוחה מוצלת בצילו של אלון בדרך לתצפית מעל הטכניון',
+    to: ''
+  },
+  {
+    title: 'בריכת המים של הטכניון',
+    caption: 'נקודת תצפית למפרץ מעל הטכניון, ממש מתחת לקו הרכבלית',
     to: ''
   },
 ]
@@ -63,6 +106,7 @@ export default {
   setup () {
     return {
       places,
+      routes,
     }
   }
 }
